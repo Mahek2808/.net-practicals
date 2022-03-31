a602ce0d77090eb25c6860e8c027dc65da79e863
Mahek <mahek.a@simformsolutions.com>
2022-03-31 13:48:13 +0530

    Pract 1

---
+++ practical1/DebuggingApplication/DebuggingApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string userResponse = Console.ReadLine();
+
+if (int.TryParse(userResponse, out int value)) 
+{
+
+    Console.WriteLine("The entered value is a integer");
+
+}
---
+++ practical1/calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace calculator
+{
+    public class calculator
+    {
+        int num1;
+        int num2;
+        int result;
+
+        public static void Main(string[] args)
+        {
+
+            calculator obj = new calculator();
+            obj.num1 = 24;
+            obj.num2 = 12;
+            Console.WriteLine("Enter first number: " + obj.num1);
+            Console.WriteLine("Enter second number: " + obj.num2);
+            Console.ReadLine();
+            obj.add();
+            obj.subsract();
+            obj.multiply();
+            obj.divide();
+
+        }
+        void add()
+        {
+            result = num1 + num2;
+            Console.WriteLine("The sum of 24 and 12 is " + result);
+            Console.ReadLine();
+        }
+
+        void subsract()
+        {
+            result = num1 - num2;
+            Console.WriteLine("The subtraction of 24 and 12 is " + result);
+            Console.ReadLine();
+        }
+
+        void multiply()
+        {
+            result = num1 * num2;
+            Console.WriteLine("The multiplication of 24 and 12 is " + result);
+            Console.ReadLine();
+        }
+
+        void divide()
+        {
+            result = num1 / num2;
+            Console.WriteLine("The division of 24 and 12 is " + result);
+            Console.ReadLine();
+        }
+    }
+}
